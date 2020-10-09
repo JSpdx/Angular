1b50be4c53de19437e82c4db89b57f07af2b1e0e
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-10-08 20:41:48 -0700

    add hero interface

--- heroesTutorial/src/app/app.component.ts
+++ heroesTutorial/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  /* Component's CSS selector */
   selector: 'app-root',
+  /* location of the component's template file */
   templateUrl: './app.component.html',
+  /* Location of the component's private style sheet */
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
